49b5f371844f9a80306a8dc5fd6c3f5d5dc690f1
Everton <evertonocarvalho@gmail.com>
2017-01-13 12:23:47 -0200

    Correções JUnit.

--- MarvelWeb/src/test/java/br/com/digitalpages/marvel/test/MarvelAppRESTControllerTest.java
+++ MarvelWeb/src/test/java/br/com/digitalpages/marvel/test/MarvelAppRESTControllerTest.java
@@ -7,13 +7,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-import java.nio.charset.Charset;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import org.junit.After;
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,8 +20,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import br.com.digitalpages.marvel.MarvelAppBoot;
-import br.com.digitalpages.marvel.dao.CharacterRepository;
-import br.com.digitalpages.marvel.model.Character;
 
 /**
  * @author Everton Carvalho [evertonocarvalho@gmail.com]
@@ -37,72 +29,57 @@ import br.com.digitalpages.marvel.model.Character;
 @WebAppConfiguration
 public class MarvelAppRESTControllerTest {
 
-    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
+	private static Logger LOGGER = Logger.getLogger(MarvelAppRESTControllerTest.class);
 
     private MockMvc mockMvc;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
 
-    @Autowired
-    private CharacterRepository characterRepository;
-
     @Before
     public void setup() throws Exception {
-        this.mockMvc = webAppContextSetup(webApplicationContext).build();
-
-        List<Character> characters = new ArrayList<>();
-
-    	for (int i = 1; i <= 10; i++) {
-    		Character character = new Character();
-    		character.setId(Long.valueOf(i));
-    		character.setName("Test Character " + i);
-    		character.setDescription("Test Character Description " + i);
-    		character.setDataModificacao(new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date()));
-    		character.setThumbnailPath("http://test.thumbnail.path." + i);
-    		character.setThumbnailExtension("jpg");
-    		characters.add(character);
-    	}
-
-    	characterRepository.save(characters);
+        mockMvc = webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
     public void listCharacters() throws Exception {
+    	LOGGER.info("listCharacters(): LISTANDO OBJETOS...");
     	mockMvc.perform(get("http://localhost:8080/characters.json/list")).andExpect(status().isOk());
+    	LOGGER.info("listCharacters(): LISTANDO OBJETOS => OK...");
     }
 
     @Test
     public void insertCharacter() throws Exception {
+    	LOGGER.info("insertCharacter(): INSERINDO OBJETO...");
         mockMvc.perform(post("http://localhost:8080/characters.json/insert")
-                .content("{\"id\":11, \"name\":\"Test Character 11\", \"description\":\"Test Character Description 11\", \"thumbnailPath\":\"http://test.thumbnail.path.11\", \"thumbnailExtension\": \"jpg\", \"dataModificacao\":\"08/12/2016 12:30\"}")
-                .contentType(contentType))
+                .content("{\"id\":1, \"name\":\"Test Character 1\", \"description\":\"Test Character Description 1\", \"thumbnailPath\":\"http://test.thumbnail.path.1\", \"thumbnailExtension\": \"jpg\", \"dataModificacao\":\"08/12/2016 12:30\"}")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
+        LOGGER.info("insertCharacter(): INSERINDO OBJETO => OK...");
     }
 
     @Test
     public void findCharacter() throws Exception {
-        mockMvc.perform(get("http://localhost:8080/characters.json/get/{id}", 11L)).andExpect(status().isOk());
+    	LOGGER.info("findCharacter(): CONSULTANDO OBJETO...");
+        mockMvc.perform(get("http://localhost:8080/characters.json/get/{id}", 1L)).andExpect(status().isOk());
+        LOGGER.info("findCharacter(): CONSULTANDO OBJETO => OK...");
     }
 
     @Test
     public void updateCharacter() throws Exception {
-        mockMvc.perform(put("http://localhost:8080/characters.json/update/{id}", 11L)
-        		.content("{\"id\":11, \"name\":\"Test Character 11 Updated\", \"description\":\"Test Character Description 11 Updated\", \"thumbnailPath\":\"http://test.thumbnail.path.11\", \"thumbnailExtension\": \"jpg\", \"dataModificacao\":\"08/12/2016 12:31\"}")
-        		.contentType(contentType))
+    	LOGGER.info("updateCharacter(): ATUALIZANDO OBJETO...");
+        mockMvc.perform(put("http://localhost:8080/characters.json/update/{id}", 1L)
+        		.content("{\"id\":1, \"name\":\"Test Character 1 Updated\", \"description\":\"Test Character Description 1 Updated\", \"thumbnailPath\":\"http://test.thumbnail.path.1\", \"thumbnailExtension\": \"jpg\", \"dataModificacao\":\"08/12/2016 12:31\"}")
+        		.contentType(MediaType.APPLICATION_JSON_UTF8))
         		.andExpect(status().isOk());
+        LOGGER.info("updateCharacter(): ATUALIZANDO OBJETO => OK...");
     }
 
     @Test
     public void deleteCharacter() throws Exception {
+    	LOGGER.info("deleteCharacter(): REMOVENDO OBJETO...");
         mockMvc.perform(delete("http://localhost:8080/characters.json/delete/{id}", 1L)).andExpect(status().isOk());
-    }
-
-    @After
-    public void deleteCharacters() throws Exception {
-    	characterRepository.deleteAll();
+        LOGGER.info("deleteCharacter(): REMOVENDO OBJETO => OK...");
     }
 
 }
